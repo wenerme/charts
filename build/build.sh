b3eb0f5757b017f5923761ea2b66e99c9a184c8c
wener <wenermail@gmail.com>
2020-07-08 19:58:22 +0800

    add more mirror

--- build/build.sh
+++ build/build.sh
@@ -5,6 +5,14 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 rm -rf ingress-nginx
 helm pull --untar ingress-nginx/ingress-nginx
 
+helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com
+rm -rf haproxy-ingress
+helm pull --untar incubator/haproxy-ingress
+
+helm repo add jetstack https://charts.jetstack.io
+rm -rf cert-manager
+helm pull --untar jetstack/cert-manager
+
 # build packages
 mkdir -p dist
 for chart in */Chart.yaml; do
