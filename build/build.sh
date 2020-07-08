072042fb664ba051787b33754f2f1378fc414378
wener <wenermail@gmail.com>
2020-07-08 18:36:40 +0800

    auto build and commit

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# update mirror
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+rm -rf ingress-nginx
+helm pull --untar ingress-nginx/ingress-nginx
+
+# build packages
 mkdir -p dist
 for chart in */Chart.yaml; do
   name=$(dirname $chart)
-  helm package -u -d dist $name
+  helm package -d dist $name
 
   mkdir -p charts/$name
   cp $name/README.md charts/$name
