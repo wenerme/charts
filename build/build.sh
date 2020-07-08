8b54e402f6bafd3452320469ef1e781f26f51c26
wener <wenermail@gmail.com>
2020-07-08 20:16:44 +0800

    fix doc build

--- build/build.sh
+++ build/build.sh
@@ -24,25 +24,30 @@ helm pull --untar hashicorp/consul
 rm -rf vault
 helm pull --untar hashicorp/vault
 
-# preprare
-# command -v $1 > /dev/null || pip install yq
-
 # build packages
 mkdir -p dist
+for chart in */Chart.yaml; do
+  name=$(dirname $chart)
+  helm package -d dist $name
+done
+rsync -av --ignore-existing --include '*.tgz' dist/ charts/
 
-echo "## Charts"  >> LIST.md
+## build doc
+
+# preprare
+command -v yq > /dev/null || pip install yq
+
+echo "## Charts"  > LIST.md
 echo "Name | Version | AppVersion"  >> LIST.md
 echo "-----|---------|-----------"  >> LIST.md
 
 for chart in */Chart.yaml; do
   name=$(dirname $chart)
-  helm package -d dist $name
 
   mkdir -p charts/$name
   cp $name/README.md charts/$name
 
   echo "$(yq r $chart 'name') | $(yq r $chart 'version') | $(yq r $chart 'appVersion')" >> LIST.md
 done
-rsync -av --ignore-existing --include '*.tgz' dist/ charts/
 
 cat README.raw.md LIST.md > README.md
