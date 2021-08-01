2e08ee91df66956bfa89447a75a4df35117470a6
陈杨文 <wenermail@gmail.com>
2021-08-02 00:31:45 +0800

    add nfs-subdir-external-provisioner

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,6 +17,8 @@ sync-chart prometheus-snmp-exporter https://prometheus-community.github.io/helm-
 sync-chart prometheus-redis-exporter https://prometheus-community.github.io/helm-charts
 sync-chart prometheus-statsd-exporter https://prometheus-community.github.io/helm-charts
 
+# nfs
+sync-chart add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
 
 # grafana
 sync-chart grafana https://grafana.github.io/helm-charts
